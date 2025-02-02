0a7151e1ec97cdb61fd858905c1ee2213ddb8377
0xhank <zeroxhank@gmail.com>
2025-02-02 15:38:51 -0500

    feat: show curve progress

--- apps/react/src/components/token/token-content.tsx
+++ apps/react/src/components/token/token-content.tsx
@@ -1,19 +1,25 @@
 import { TokenTradeForm } from "./token-trade-form";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useTokenData } from "../../hooks/useTokenData";
 import supabase from "../../sbClient";
 import TokenCard from "../common/token-card";
-import { TokenBalance } from "./token-balance";
 import { LineChart } from "./line-chart";
+import { TokenBalance } from "./token-balance";
 
 export const TokenContent = ({ mint }: { mint: string }) => {
     const { tokenData } = useTokenData({ mint });
-    const [tokenPrices, setTokenPrices] = useState<{ time: string; value: number }[]>([]);
+    const [tokenPrices, setTokenPrices] = useState<
+        { time: string; value: number }[]
+    >([]);
     const [loading, setLoading] = useState(true);
 
+    const [curveLiquidity, setCurveLiquidity] = useState<number | null>(null);
+
+    // todo: only fetch prices from a certain window
+    // todo: convert the prices into candles
     const fetchTokenPrices = async () => {
-        const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000).toISOString();
+        // const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000).toISOString();
         const { data, error } = await supabase
             .from("token_price_usd")
             .select("price_usd, created_at")
@@ -24,13 +30,40 @@ export const TokenContent = ({ mint }: { mint: string }) => {
         if (error) {
             console.error(error);
         } else {
-            setTokenPrices(data.map((price) => ({ time: price.created_at, value: price.price_usd })));
+            setTokenPrices(
+                data.map((price) => ({
+                    time: price.created_at,
+                    value: price.price_usd,
+                }))
+            );
             setLoading(false);
+        }
+    };
+
+    const progress = useMemo(() => {
+        if (!curveLiquidity) return null;
+        const initialLiquidity = 793_100_000_000_000;
+        return (initialLiquidity - curveLiquidity) / initialLiquidity;
+    }, [curveLiquidity]);
+
+    const fetchCurveLiquidity = async () => {
+        const { data, error } = await supabase
+            .from("curve_data")
+            .select("virtual_token_reserves")
+            .eq("mint", mint)
+            .order("created_at", { ascending: false })
+            .limit(1);
+
+        if (error) {
+            console.error(error);
+        } else {
+            setCurveLiquidity(data[0].virtual_token_reserves);
         }
     };
 
     useEffect(() => {
         fetchTokenPrices();
+        fetchCurveLiquidity();
     }, [mint]);
 
     return (
@@ -40,6 +73,26 @@ export const TokenContent = ({ mint }: { mint: string }) => {
                 <div className="flex justify-center w-full">
                     <TokenCard token={tokenData} clickable={false} />
                 </div>
+
+                {/* Progress Bar */}
+                {progress !== null && (
+                    <div className="w-full flex flex-col gap-2">
+                        <div className="flex justify-between text-sm">
+                            <span>Curve Progress</span>
+                            <div>
+                                <span className="text-red-600 font-bold">
+                                    {(progress * 100).toFixed(2)}%
+                                </span>
+                            </div>
+                        </div>
+                        <div className="w-full bg-gray-200 rounded-full h-2.5">
+                            <div
+                                className="bg-red-600 h-2.5 rounded-full"
+                                style={{ width: `${progress * 100}%` }}
+                            />
+                        </div>
+                    </div>
+                )}
 
                 {/* Price Graph */}
                 {loading ? (
