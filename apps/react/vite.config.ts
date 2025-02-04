0d8ced92166e212deb9326b4a778e8e5d377f9c0
0xhank <zeroxhank@gmail.com>
2025-02-03 19:46:08 -0500

    fix: add styles back again

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -1,10 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         react(),
         nodePolyfills({
             // Whether to polyfill specific globals.
