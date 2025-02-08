23fb8bced2111b2aeabc5f2c9a52699d441d1e0b
0xhank <zeroxhank@gmail.com>
2025-02-08 15:56:07 -0500

    feat: improve maintenance page

--- apps/react/src/pages/maintenance.tsx
+++ apps/react/src/pages/maintenance.tsx
@@ -1,29 +1,7 @@
-import bs58 from "bs58";
-import React, { useState } from "react";
 
 export default function MaintenancePage() {
-    const [address, setAddress] = useState("");
-    const [isValid, setIsValid] = useState(true);
-    const isBs58 = (id: string) => {
-        try {
-            bs58.decode(id);
-            return true;
-        } catch {
-            return false;
-        }
-    };
-    const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setAddress(value);
-        setIsValid(value === "" || (value.length === 44 && isBs58(value)));
-    };
-
-    const handleSubmit = () => {
-        if (!isValid) {
-            return;
-        }
-        console.log(address);
-    };
+  
+  
 
     return (
         <div className="relative min-h-screen w-full">
@@ -32,34 +10,58 @@ export default function MaintenancePage() {
                 style={{ backgroundImage: 'url("/cloud-bg.webp")' }}
             />
             <div className="relative flex min-h-screen flex-col items-center justify-center gap-4">
-                <h1 className="text-[80pt] lg:text-[100pt] -mb-8">
-                    <span className="font-serif font-semibold -mb-8">Profiteer</span>
+                <h1 className="text-[80pt] flex flex-col lg:flex-row lg:gap-2 text-center lg:text-[100pt] -mb-8">
+                    <span className="font-serif font-semibold -mb-12 lg:-mb-8">
+                        Profiteer{" "}
+                    </span>
+                    <span className="font-script font-semibold -mb-8 text-accent">
+                        News
+                    </span>
                 </h1>
-                        <p className="text-sm text-red-500 h-4">
-
-                    {!isValid && "Invalid Solana address"}
-                    {isValid && <p>{ }</p>}
-                </p>
-                <div className="w-full max-w-md px-4">
-                    <input
-                        type="text"
-                        value={address}
-                        onChange={handleAddressChange}
-                        placeholder="Enter Solana wallet address"
-                        className={`w-full rounded-lg border p-2 bg-white/50 ${
-                            isValid ? "border-gray-300" : "border-red-500"
-                        }`}
-                    />
-
+                
+                <div className="mt-8 text-center bg-gray-400/30 p-8 backdrop-blur-xl max-w-xl mx-auto">
+                    <h2 className="text-3xl font-bold mb-6 text-white">
+                        Join the alpha group
+                    </h2>
+                    <ol className="list-decimal text-left space-y-4 max-w-md mx-auto pl-6">
+                        <li className="text-lg text-white font-semibold">
+                            Follow{" "}
+                            <a
+                                href="https://x.com/0xhank"
+                                className="text-accent! hover:text-accent/80 transition-colors font-bold hover:underline!"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                0xhank
+                            </a>{" "}
+                            and{" "}
+                            <a
+                                href="https://x.com/profiteernews"
+                                className="text-accent! hover:text-accent/80 transition-colors font-bold hover:underline!"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                profiteernews
+                            </a>{" "}
+                            on X
+                        </li>
+                        <li className="text-lg text-white font-semibold">
+                            Repost{" "}
+                            <a
+                                href="https://x.com/profiteernews/status/1888328591504462186"
+                                className="text-accent! hover:text-accent/80 transition-colors font-bold hover:underline!"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                this post on X
+                            </a>
+                        </li>
+                        <li className="text-lg text-white font-semibold">
+                            DM with your Telegram handle
+                        </li>
+                    </ol>
                 </div>
-                <button
-                    onClick={handleSubmit}
-                    className="btn btn-accent text-black"
-                    // disabled={address === "" || !isValid}
-                    disabled={true}
-                >
-                    Alpha crew closed
-                </button>
+
                 <a
                     href="https://x.com/profiteernews"
                     target="_blank"
