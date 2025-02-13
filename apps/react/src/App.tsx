01716429cff9405fefc2ef2b37602c0614e231a3
0xhank <zeroxhank@gmail.com>
2025-02-13 12:06:21 -0500

    chore: add skip login override

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -2,23 +2,23 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import LoadingScreen from "./components/common/loading-screen";
 import TopBar from "./components/top-bar/top-bar";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import { PortfolioProvider } from "./contexts/PortfolioContext";
+import { PreviewProvider } from "./contexts/PreviewContext";
 import { SolPriceProvider } from "./contexts/SolPriceContext";
 import { TokenProvider } from "./contexts/TokenProvider";
 import { Page404 } from "./pages/404";
+import Admin from "./pages/admin";
 import Home from "./pages/home";
+import Login from "./pages/login";
+import Maintenance from "./pages/maintenance";
+import { Privacy } from "./pages/privacy";
+import { Terms } from "./pages/terms";
 import Token from "./pages/topic";
 import { PrivyClientProvider } from "./providers/privy-client-provider";
 import { ServerProvider } from "./providers/server-provider";
-import Admin from "./pages/admin";
-import Maintenance from "./pages/maintenance";
-import { PreviewProvider } from "./contexts/PreviewContext";
-import { AuthProvider, useAuth } from "./contexts/AuthContext";
-import Login from "./pages/login";
-import { Privacy } from "./pages/privacy";
-import { Terms } from "./pages/terms";
-import LoadingScreen from "./components/common/loading-screen";
 function App() {
     if (import.meta.env.VITE_MAINTENANCE) {
         return <Maintenance />;
@@ -54,14 +54,17 @@ function App() {
 
 function AppContent() {
     const { hasAccess, ready } = useAuth();
-    if (!ready && !import.meta.env.VITE_SKIP_INVITE) return <LoadingScreen />;
-    if (!hasAccess && !import.meta.env.VITE_SKIP_INVITE) return <Login />;
+    const skipLogin =
+        new URLSearchParams(window.location.search).get("skipLogin") === "true";
+
+    if (!ready && !import.meta.env.VITE_SKIP_INVITE && !skipLogin)
+        return <LoadingScreen />;
+    if (!hasAccess && !import.meta.env.VITE_SKIP_INVITE && !skipLogin)
+        return <Login />;
     return (
         <div className="flex flex-col items-center h-screen w-screen absolute overflow-y-auto top-0 left-0 right-0 z-50 bg-gray-100">
             <TopBar />
-            <div
-                className="flex justify-center w-full h-full "
-            >
+            <div className="flex justify-center w-full h-full ">
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/wiki/:id" element={<Token />} />
